88de9abad4479fd07581fb9f21683b4ebe6ee09c
Shravan Kamalaker <shravankamalaker@gmail.com>
2022-09-29 23:14:11 +0300

    Add existing files to git

---
+++ CRUD DEMO/controllers/employeeController.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const { Employee } = require('../models/employee');
+var router = express.Router();
+var ObjectId = require('mongoose').Types.ObjectId;
+
+router.get('/',(req,res) => {
+    Employee.find((err, docs) => {
+        if(!err){res.send(docs);}
+        else {
+            console.log('Error in Retrieving Employees : '+JSON.stringify(err, undefined,2));
+        }
+    })
+});
+
+router.post('/',(req,res)=>{
+    var emp = new Employee({
+        name : req.body.name,
+        position: req.body.position,
+        office : req.body.office,
+        salary: req.body.salary,
+    });
+    emp.save((err,doc)=> {
+        if(!err){res.send(doc)}
+        else {console.log('Error in saving Employee : '+JSON.stringify(err, undefined,2));}
+    });
+});
+
+router.put('/',(req,res)=>{
+    if(!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with param id : ${req.params.id}`);
+
+    var emp = new Employee({
+        name : req.body.name,
+        position: req.body.position,
+        office : req.body.office,
+        salary: req.body.salary,
+    });
+    emp.findByIdAndUpdate(req.params.id, {$set: emp}, {new : true}, (err,doc)=> {
+        if(!err){res.send(doc)}
+        else {console.log('Error in Employee Update: '+JSON.stringify(err, undefined,2));}
+    });
+});
+
+router.delete('/:id',(req,res)=>{
+    if(!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with param id : ${req.params.id}`);
+
+    emp.findByIdAndRemove(req.params.id, (err,doc)=> {
+        if(!err){res.send(doc)}
+        else {console.log('Error in Employee Delete: '+JSON.stringify(err, undefined,2));}
+    });
+});
+module.exports = router;
